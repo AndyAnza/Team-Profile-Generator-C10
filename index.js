4036cbc79b45e23321f3762765a4e1c817299578
Andrea Ramirez <andrea.anza3@gmail.com>
2023-02-20 13:36:59 -0600

    Installed jest and inquirer, updated gitignore

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const inquirer = require(inquirer);
-
-
-//TODO: writeToFile with user input
-function writeToFile () {
-
-}
-
-//TODO: Generate HTML adding user input
-function generateHtml () {
-
-}
+const fs = require('fs');
+const inquirer = require('inquirer');
