76988aaacb0830e0155c4670ab2d48e9af6f727d
Andrea Ramirez <andrea.anza3@gmail.com>
2023-02-20 01:24:00 -0600

    Added starter files

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require(inquirer);
+
+
+let manager = {
+    'name' : '',
+    'employee ID': '',
+    'email': '',
+    'office number': ''
+}
+
+let engineer = {
+    'name' : '',
+    'ID' : '',
+    'email' : '',
+    'GitHub username' : '',
+}
+
+let intern = {
+    'name' : '',
+    'ID' : '',
+    'email' : '',
+    'school' : '',
+}
+
+//TODO: writeToFile with user input
+function writeToFile () {
+
+}
+
+//TODO: Generate HTML adding user input
+function generateHtml () {
+
+}
+
